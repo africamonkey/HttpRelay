12de0fe8626e6b4a42b46edb9a097852bc6b5027
africamonkey <marses@vip.qq.com>
2026-04-23 22:52:29 +0800

    Initial Commit

---
+++ HttpRelay/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HttpRelay
+//
+//  Created by Africamonkey on 2026/4/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HttpRelayTests/HttpRelayTests.swift
@@ -0,0 +1,17 @@
+//
+//  HttpRelayTests.swift
+//  HttpRelayTests
+//
+//  Created by Africamonkey on 2026/4/23.
+//
+
+import Testing
+@testable import HttpRelay
+
+struct HttpRelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
