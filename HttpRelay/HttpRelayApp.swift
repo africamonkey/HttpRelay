9b403d8a2c5879d1cae29d7a7f6214a080fc33e2
africamonkey <marses@vip.qq.com>
2026-09-17 23:13:33 +0800

    feat: react to scenePhase via BackgroundKeepaliveCoordinator

--- HttpRelay/HttpRelayApp.swift
+++ HttpRelay/HttpRelayApp.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 @main
 struct HttpRelayApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            switch newPhase {
+            case .background:
+                BackgroundKeepaliveCoordinator.shared.start()
+            case .active:
+                BackgroundKeepaliveCoordinator.shared.stop()
+            case .inactive:
+                break
+            @unknown default:
+                break
+            }
         }
     }
 }
